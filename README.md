875177c870d16af7f46878fa06c1f6649b3a1b0d
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2021-11-18 11:41:46 +0530

    navbar added

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Pokemon Go Dictionary React app
 
+Project is for practice and production upload site.
 To give all details of pokemon available in game - pokemon go.
 
+## Contains:
+1. API fetching to show list of all pokemon Go - Pokemons
+2. Own single page to show the details of each pokemon of game.
+3. Every page is routed with same code in **React app.** 
+
+\
+\
+\
+\
+\
 
 # Getting Started with Create React App
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import siteIcon from './images_Used/pokeBall.png'
 
 function App() {
+  const [navList, setNavList] = useState("hamParent")
+  const [activeInNavList, setActiveInNavList] = useState(true)
+  var showList = () => {
+    if (activeInNavList) {
+      setActiveInNavList = !activeInNavList
+      setNavList('hamParent active')
+    } else {
+      console.log("hamParent");
+      setNavList('hamParent')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with ujjwal
-        </a>
-      </header>
-    </div>
+    <div>
+      <nav className="navbar">
+        <div style={{ padding: '4px' }}>
+          <a href="#"><img className="primary-icon" src={siteIcon} alt="Logo " /></a>
+        </div>
+        <div className={navList} id="hamParent" onClick={() => showList}>
+          <div className="hamLine"></div>
+          <div className="hamLine"></div>
+          <div className="hamLine"></div>
+        </div>
+        <ul className="nav-list" id="nav-list">
+          <li className="list-item"><a href="#">Home</a></li>
+          <li className="list-item"><a href="#">Pokemon</a></li>
+          <li className="list-item"><a href="#">About</a></li>
+          <li className="list-item"><a href="#">Support</a></li>
+          <li className="list-item"><a href="#">Contact</a></li>
+          <li className="list-item"><a href="#">Join</a></li>
+        </ul>
+      </nav >
+
+      <div className="mainBody">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores quibusdam architecto unde magnam veniam illo
+        ad ipsa voluptatibus? Sed quisquam, commodi autem cupiditate aperiam exercitationem, nam aspernatur temporibus
+        explicabo debitis minima magni reiciendis.
+        <br />
+        <h3>
+          From here we will add the body to the app.
+        </h3>
+      </div>
+
+    </div >
   );
 }
 
